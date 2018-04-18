08879a25989856f6e890bbc5cba80e9850eeedec
Liam Cavens <liamcavens@googlemail.com>
2018-04-18 11:55:35 +0100

    made save method

--- models/students_methods.rb
+++ models/students_methods.rb
@@ -12,4 +12,18 @@ class Student
         @age = options["age"].to_i
     end
 
+    def full_name()
+        return "#{@first_name} #{@last_name}"
+    end
+
+    def save()
+        sql = "INSERT INTO students
+         (first_name, last_name, house, age)
+          VALUES
+           ($1, $2, $3, $4) 
+            RETURNING *"
+        values = [@first_name, @last_name, @house, @age]
+        student_info = SqlRunner.run(sql, values)
+        @id = student_info.first()["id"].to_i
+    end
 end
